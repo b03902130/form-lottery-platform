706bdd4f0e76023b4d7acf424c92e21c06fa2792
sankarshanarao <sankarshanarao@github.com>
2018-06-29 23:53:15 +0530

    Minor Adjustments

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.listen(port, '0.0.0.0', (err) => {
     console.log(err);
   }
 
-  console.info('>>> 🌎 Open http://0.0.0.0:%s/ in your browser.', port);
+  console.info('Open http://localhost:%s/ in your browser.', port);
 });
 
 module.exports = app;
