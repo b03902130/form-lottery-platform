4d4a927555f50885d29083b89884549acfa0b61d
b03902130 <b03902130@ntu.edu.tw>
2019-06-03 07:08:49 +0800

    modify debugger & change to localhost

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-window.BACKEND = 'http://linux3.csie.ntu.edu.tw:9090'
+window.BACKEND = 'http://localhost:9090'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
