b7e4e8fe8f63619b34be026cbd4de4ba3143defe
Leo Yang <leo19941227@gmail.com>
2019-06-13 00:07:38 +0800

    build production

---
+++ build/precache-manifest.e9623a54d527b41638fee03ed6a63bbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc9d4fbae0a7b4b34fda",
+    "url": "/static/js/main.cc9d4fba.chunk.js"
+  },
+  {
+    "revision": "04d294ca22a27cd73010",
+    "url": "/static/js/1.04d294ca.chunk.js"
+  },
+  {
+    "revision": "cc9d4fbae0a7b4b34fda",
+    "url": "/static/css/main.dca77b8f.chunk.css"
+  },
+  {
+    "revision": "03810fdccbc966b9271427c866e2986c",
+    "url": "/index.html"
+  }
+];
